2e28a925cafdf67dbf6d45611d6cf4cdaf76fe8e
Michael B <bosse730@gmail.com>
2022-12-18 01:42:37 +1100

    past present and future classes show based on time

--- Develop/script.js
+++ Develop/script.js
@@ -5,7 +5,7 @@
 // TODO
 // Call day.js API to get date at top of page                DONE
 // Add additional business hours blocks for each hour of the working day 9am-5pm         DONE
-// compare each time hour block with the current time and change style of block based on if it has been passed
+// compare each time hour block with the current time and change style of block based on if it has been passed      DONE
 // set save button to store input data into local storage so it remains even after refresh
 
 var currentDate = dayjs().format("dddd, MMMM D");
@@ -18,7 +18,45 @@ today.text(currentDate);
 var currentTime = dayjs().format("HH:mm:");
 console.log(currentTime);
 
+
+
 $(function () {
+  var divElement = $("div.row");
+
+  console.log(divElement);
+
+  for (i = 0; i<divElement.length; i++) {
+    
+    if (dayjs().format("HH")>i+9) {
+      divElement.eq(i).addClass("past")  
+    } else if (dayjs().format("HH")==i+9) {
+      divElement.eq(i).addClass("present") 
+    } else if (dayjs().format("HH")<i+9) {
+      divElement.eq(i).addClass("future") 
+    }
+  }
+  
+  var buttonEl = $("div").children("button");
+
+  buttonEl.on("click", function () {
+
+    for(i = 0; i = this.length; i++) {
+      var textInput = $("div").children("textarea").attr("textContent");
+  
+      if (textInput(i)!=="") {
+        textInput.eq(i) = textInput;
+        localStorage.setItem("textInput.eq(i)",textInput);
+      } 
+  
+      textInput = localStorage.getItem("textInput.eq(i)");
+    }
+  })
+  
+
+  // var textInput = $("div").children("textarea").attr("textContent");
+
+  // console.log(textInput);
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
