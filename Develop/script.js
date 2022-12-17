e9ccad21effc65c3856f2e53ebd6699fe0c72c3d
Michael B <bosse730@gmail.com>
2022-12-17 23:37:29 +1100

    linked date to html and created time variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,23 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+// TODO
+// Call day.js API to get date at top of page                DONE
+// Add additional business hours blocks for each hour of the working day 9am-5pm         DONE
+// compare each time hour block with the current time and change style of block based on if it has been passed
+// set save button to store input data into local storage so it remains even after refresh
+
+var currentDate = dayjs().format("dddd, MMMM D");
+console.log(currentDate);
+
+var today = $("#currentDay");
+
+today.text(currentDate);
+
+var currentTime = dayjs().format("HH:mm:");
+console.log(currentTime);
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
